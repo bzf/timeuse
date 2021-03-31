862230bdadb367714d8f2d2254dc3a63c216b591
André Ligné <andre@ligne.io>
2021-03-31 13:03:35 +0200

    Redirect from / to /sign-in

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function () {
+  this.route('index', { path: '' });
   this.route('sign-in');
 
   this.route('authenticated', { path: '' }, function () {
