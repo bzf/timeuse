f5add28bc2715a2cadfb3bf85eac0bbecc125d87
André Ligné <andre@ligne.io>
2021-04-01 08:00:46 +0200

    Show timers on the dashboard route

--- app/models/timer.js
+++ app/models/timer.js
@@ -17,4 +17,20 @@ export default class TimerModel extends Model {
   stop() {
     this.endTimestamp = new Date();
   }
+
+  get durationText() {
+    const totalDurationInSeconds = Math.round(
+      Date.parse(this.endTimestamp) / 1000 -
+        Date.parse(this.startTimestamp) / 1000
+    );
+
+    const hours = Math.floor(totalDurationInSeconds / 3600);
+    const minutes = Math.floor((totalDurationInSeconds % 3600) / 60);
+    const seconds = totalDurationInSeconds % 60;
+
+    return [hours, minutes, seconds]
+      .map((a) => String(a))
+      .map((a) => a.padStart(2, '0'))
+      .join(':');
+  }
 }
---
+++ app/routes/dashboard.js
@@ -0,0 +1,8 @@
+import Route from '@ember/routing/route';
+
+export default class extends Route {
+  async model() {
+    const timers = await this.store.findAll('timer');
+    return timers.rejectBy('isRunning');
+  }
+}
