d4e6b89537c3047894d68892fb7fd5eac90867b2
André Ligné <andre@ligne.io>
2021-04-02 22:44:15 +0200

    Convert DailyTimers to Typecscript

--- app/models/daily-timers.ts
+++ app/models/daily-timers.ts
@@ -1,19 +1,20 @@
 import { tracked } from '@glimmer/tracking';
+import TimerModel from 'timeuse/models/timer';
 
 export default class DailyTimers {
-  date = null;
-  @tracked _timers = [];
+  date: string;
+  @tracked _timers: Array<TimerModel> = [];
 
-  constructor(date, timers) {
+  constructor(date: string, timers: Array<TimerModel>) {
     this.date = date;
     this._timers = timers;
   }
 
-  get timers() {
+  get timers(): Array<TimerModel> {
     return this._timers.sortBy('endTimestamp').rejectBy('isDeleted').reverse();
   }
 
-  get totalDuration() {
+  get totalDuration(): number {
     return this.timers
       .mapBy('durationInSeconds')
       .reduce((acc, a) => acc + a, 0);
