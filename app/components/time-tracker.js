0ee2a4c7556c1fb87b3266494d6ce74e00eee5b6
André Ligné <andre@ligne.io>
2021-04-02 18:50:03 +0200

    Keep track of the current timer in a Service

--- app/components/time-tracker.js
+++ app/components/time-tracker.js
@@ -1,67 +1,30 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { dropTask } from 'ember-concurrency';
 import { inject as service } from '@ember/service';
-import { isEmpty } from '@ember/utils';
-import moment from 'moment';
 
 export default class extends Component {
-  @service store;
+  @service('current-timer') timerService;
 
-  @tracked title = '';
-  @tracked currentTime = new Date();
-  @tracked currentTimer = null;
+  get currentTimer() {
+    return this.timerService.currentTimer;
+  }
 
-  @dropTask
-  *setupCurrentTimer() {
-    const currentTimers = yield this.store.query('timer', {
-      end_timestamp: 'is.null',
-      start_timestamp: 'not.is.null',
-    });
-
-    this.currentTimer = currentTimers.firstObject;
-
-    if (isEmpty(this.currentTimer)) {
-      this.currentTimer = this.store.createRecord('timer');
-    }
+  get currentDuration() {
+    return this.timerService.currentDuration;
   }
 
   @action
   async startTimer() {
-    if (this.currentTimer.isRunning) {
-      return;
-    }
-
-    this.currentTimer.start();
-    await this.currentTimer.save();
+    await this.timerService.start();
   }
 
   @action
   async stopTimer() {
-    this.currentTimer.stop();
-    await this.currentTimer.save();
-
-    this.currentTimer = this.store.createRecord('timer');
+    await this.timerService.stop();
   }
 
   @action
   async saveTimer() {
-    if (this.currentTimer.isNew) {
-      return;
-    }
-
-    await this.currentTimer.save();
-  }
-
-  @action
-  setupTimer() {
-    setInterval(() => (this.currentTime = new Date()), 500);
-  }
-
-  get currentDuration() {
-    return moment
-      .duration(moment(this.currentTime).diff(this.currentTimer.startTimestamp))
-      .as('seconds');
+    await this.timerService.save();
   }
 }
--- app/routes/authenticated.js
+++ app/routes/authenticated.js
@@ -3,10 +3,15 @@ import { inject as service } from '@ember/service';
 
 export default class extends Route {
   @service supabase;
+  @service currentTimer;
 
   beforeModel() {
     if (!this.supabase.isAuthenticated) {
       this.transitionTo('sign-in');
     }
+  }
+
+  afterModel() {
+    this.currentTimer.setup();
   }
 }
