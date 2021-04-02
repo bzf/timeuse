bdd4cda7a4cc292a4ec542cdf532e8a4cc9269b4
André Ligné <andre@ligne.io>
2021-04-02 19:14:49 +0200

    Remove service from TimeTracker and render in the dashboard route

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -3,14 +3,25 @@ import { action } from '@ember/object';
 import moment from 'moment';
 
 import DailyTimers from 'timeuse/models/daily-timers';
+import { inject as service } from '@ember/service';
 
 export default class extends Controller {
+  @service('current-timer') timerService;
+
   get timers() {
     return this.model;
   }
 
   get currentDate() {
     return moment().format('YYYY-MM-DD');
+  }
+
+  get currentTimer() {
+    return this.timerService.currentTimer;
+  }
+
+  get currentDuration() {
+    return this.timerService.currentDuration;
   }
 
   get timersGroupedByDate() {
@@ -34,5 +45,20 @@ export default class extends Controller {
   async deleteTimer(timer) {
     this.model = this.model.rejectBy('id', timer.id);
     await timer.destroyRecord();
+  }
+
+  @action
+  async startTimer() {
+    await this.timerService.start();
+  }
+
+  @action
+  async stopTimer() {
+    await this.timerService.stop();
+  }
+
+  @action
+  async saveTimer() {
+    await this.timerService.save();
   }
 }
