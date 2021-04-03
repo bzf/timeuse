49f49f0b9cb1784d19156cb63e66ce98817a38f1
André Ligné <andre@ligne.io>
2021-04-03 22:57:18 +0200

    Add a sidebar with the timer going

--- app/controllers/authenticated.js
+++ app/controllers/authenticated.js
@@ -4,6 +4,7 @@ import { inject as service } from '@ember/service';
 
 export default class extends Controller {
   @service supabase;
+  @service currentTimer;
 
   @action
   async signOut() {
--- config/icons.js
+++ config/icons.js
@@ -10,6 +10,7 @@ module.exports = function () {
       'check',
       'sign-out-alt',
       'search',
+      'clock',
     ],
   };
 };
